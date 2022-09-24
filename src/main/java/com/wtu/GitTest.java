f6e805c4225fb2dc7d7d12435a40da6fe10be566
zhang <1229944289@qq.com>
2022-09-24 17:00:54 +0800

    hot-fix2 commit

--- src/main/java/com/wtu/GitTest.java
+++ src/main/java/com/wtu/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git01");
         System.out.println("git02");
-        System.out.println("hot-fix1");
+        System.out.println("git03");
+        System.out.println("git04");
+        System.out.println("git05");
     }
 }
