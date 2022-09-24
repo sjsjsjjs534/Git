131dd2dbf32725c6a7be4bfee9dca95c39b38bdc
张宇若 <76990463+sjsjsjjs534@users.noreply.github.com>
2022-09-24 17:35:19 +0800

    pull commit

--- src/main/java/com/wtu/GitTest.java
+++ src/main/java/com/wtu/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("git04");
         System.out.println("hotfix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
